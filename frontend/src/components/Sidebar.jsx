2de4f8997b1e9134eadeb1bac9ddd19b5e9aeb3c
hammad872 <hammadsiddiq98@gmail.com>
2024-03-06 14:47:16 +0500

    habibasexy

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { Tooltip, Button } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  const [open, setOpen] = React.useState(0);
+  const [tooltips, setTooltips] = React.useState({
+    button1: false,
+    button2: false,
+    button3: false,
+    button4: false,
+  });
 
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value);
+  const toggleTooltip = (button) => {
+    setTooltips((prevState) => ({
+      ...prevState,
+      [button]: !prevState[button],
+    }));
+  };
+
+  const navigate = useNavigate();
+
+  const sales = () => {
+    navigate("/sales");
   };
 
   return (
@@ -17,87 +31,122 @@ const Sidebar = () => {
             className="rounded-full w-10 h-10 mb-3 mx-auto"
           />
         </a>
-        
+
         <div className="mt-10">
           <ul>
             <li className="mb-6 flex justify-center">
-            <Tooltip title="Add" placement="right">
-              <Button className="p-3">
-                <span>
-                  <svg
-                    className="fill-current h-5 w-5 text-gray-300 mx-auto hover:text-green-500 "
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M14 3V3.28988C16.8915 4.15043 19 6.82898 19 10V17H20V19H4V17H5V10C5 6.82898 7.10851 4.15043 10 3.28988V3C10 1.89543 10.8954 1 12 1C13.1046 1 14 1.89543 14 3ZM7 17H17V10C17 7.23858 14.7614 5 12 5C9.23858 5 7 7.23858 7 10V17ZM14 21V20H10V21C10 22.1046 10.8954 23 12 23C13.1046 23 14 22.1046 14 21Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </span>
-              </Button>
-            </Tooltip>
-
+              <div
+                className="tooltip"
+                onMouseEnter={() => toggleTooltip("button1")}
+                onMouseLeave={() => toggleTooltip("button1")}
+              >
+                <button className="p-3">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 text-gray-300 mx-auto hover:text-green-500"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z"
+                      />
+                    </svg>
+                  </span>
+                </button>
+                {tooltips.button1 && (
+                  <span className="tooltiptext">Dashboard</span>
+                )}
+              </div>
             </li>
             <li className="mb-6 flex justify-center">
-              <Button className="p-3">
-                <span>
-                  <svg
-                    className="fill-current h-5 w-5 mx-auto text-gray-300 hover:text-green-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M12 4a4 4 0 014 4 4 4 0 01-4 4 4 4 0 01-4-4 4 4 0
-                      014-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4
-                      8-4z"
-                    ></path>
-                  </svg>
-                </span>
-              </Button>
+              <div
+                className="tooltip"
+                onMouseEnter={() => toggleTooltip("button2")}
+                onMouseLeave={() => toggleTooltip("button2")}
+              >
+                <button className="p-3">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 text-gray-300 mx-auto hover:text-green-500"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                      />
+                    </svg>
+                  </span>
+                </button>
+                {tooltips.button2 && (
+                  <span className="tooltiptext">Button 1 Tooltip</span>
+                )}
+              </div>
             </li>
             <li className="mb-6 flex justify-center">
-              <Button className="p-3">
-                <span>
-                  <svg
-                    className="fill-current h-5 w-5 text-gray-300 mx-auto hover:text-green-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M12 13H7v5h5v2H5V10h2v1h5v2M8
-                      4v2H4V4h4m2-2H2v6h8V2m10 9v2h-4v-2h4m2-2h-8v6h8V9m-2
-                      9v2h-4v-2h4m2-2h-8v6h8v-6z"
-                    ></path>
-                  </svg>
-                </span>
-              </Button>
+              <div
+                className="tooltip"
+                onMouseEnter={() => toggleTooltip("button3")}
+                onMouseLeave={() => toggleTooltip("button3")}
+              >
+                <button className="p-3" onClick={sales}>
+                  <span>
+                    <svg
+                      className="fill-current h-5 w-5 text-gray-300 mx-auto hover:text-green-500 "
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M14 3V3.28988C16.8915 4.15043 19 6.82898 19 10V17H20V19H4V17H5V10C5 6.82898 7.10851 4.15043 10 3.28988V3C10 1.89543 10.8954 1 12 1C13.1046 1 14 1.89543 14 3ZM7 17H17V10C17 7.23858 14.7614 5 12 5C9.23858 5 7 7.23858 7 10V17ZM14 21V20H10V21C10 22.1046 10.8954 23 12 23C13.1046 23 14 22.1046 14 21Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </span>
+                </button>
+                {tooltips.button3 && (
+                  <span className="tooltiptext">Button 3 Tooltip</span>
+                )}
+              </div>
             </li>
-            <li className="flex justify-center">
-              <Button className="p-3">
-                <span>
-                  <svg
-                    className="fill-current h-5 w-5 text-gray-300 mx-auto hover:text-green-500"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M22.775 8C22.9242 8.65461 23 9.32542 23 10H14V1C14.6746 1 15.3454 1.07584 16 1.22504C16.4923 1.33724 16.9754 1.49094 17.4442 1.68508C18.5361 2.13738 19.5282 2.80031 20.364 3.63604C21.1997 4.47177 21.8626 5.46392 22.3149 6.55585C22.5091 7.02455 22.6628 7.5077 22.775 8ZM20.7082 8C20.6397 7.77018 20.5593 7.54361 20.4672 7.32122C20.1154 6.47194 19.5998 5.70026 18.9497 5.05025C18.2997 4.40024 17.5281 3.88463 16.6788 3.53284C16.4564 3.44073 16.2298 3.36031 16 3.2918V8H20.7082Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M1 14C1 9.02944 5.02944 5 10 5C10.6746 5 11.3454 5.07584 12 5.22504V12H18.775C18.9242 12.6546 19 13.3254 19 14C19 18.9706 14.9706 23 10 23C5.02944 23 1 18.9706 1 14ZM16.8035 14H10V7.19648C6.24252 7.19648 3.19648 10.2425 3.19648 14C3.19648 17.7575 6.24252 20.8035 10 20.8035C13.7575 20.8035 16.8035 17.7575 16.8035 14Z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                </span>
-              </Button>
+            <li className="mb-6 flex justify-center">
+              <div
+                className="tooltip"
+                onMouseEnter={() => toggleTooltip("button4")}
+                onMouseLeave={() => toggleTooltip("button4")}
+              >
+                <button className="p-3">
+                  <span>
+                    <svg
+                      className="fill-current h-5 w-5 text-gray-300 mx-auto hover:text-green-500 "
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M14 3V3.28988C16.8915 4.15043 19 6.82898 19 10V17H20V19H4V17H5V10C5 6.82898 7.10851 4.15043 10 3.28988V3C10 1.89543 10.8954 1 12 1C13.1046 1 14 1.89543 14 3ZM7 17H17V10C17 7.23858 14.7614 5 12 5C9.23858 5 7 7.23858 7 10V17ZM14 21V20H10V21C10 22.1046 10.8954 23 12 23C13.1046 23 14 22.1046 14 21Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  </span>
+                </button>
+                {tooltips.button4 && (
+                  <span className="tooltiptext">Button 1 Tooltip</span>
+                )}
+              </div>
             </li>
           </ul>
         </div>
@@ -106,7 +155,7 @@ const Sidebar = () => {
         <a href="#">
           <span>
             <svg
-              className="fill-current h-5 w-5 text-gray-300 mx-auto hover:text-red-500"
+              className="fill-current hx-5 w-5 text-gray-300 mx-auto hover:text-red-500"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
